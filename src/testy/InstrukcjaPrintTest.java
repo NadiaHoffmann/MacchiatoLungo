7057bf4ad251d471ce655b60e857b4249f978249
Nadia Hoffmann <nh448252@students.mimuw.edu.pl>
2023-06-18 17:49:41 +0200

    Dodaj testy dla instrukcji if

--- src/testy/InstrukcjaPrintTest.java
+++ src/testy/InstrukcjaPrintTest.java
@@ -1,8 +1,42 @@
 package testy;
 
+import instrukcje.BudowniczyProgramu;
+import instrukcje.Program;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import wyrazenia.Dodawanie;
+import wyrazenia.Literal;
+import wyrazenia.Zmienna;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class InstrukcjaPrintTest {
+    private final PrintStream standardoweWyjscie = System.out;
+    private final ByteArrayOutputStream ciagNaWyjsciu = new ByteArrayOutputStream();
 
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(ciagNaWyjsciu));
+    }
+
+
+    @Test
+    public void TestBlockPrints(){
+        Program program = new BudowniczyProgramu()
+                .zadeklarujZmienna('x', Literal.oWartosci(2137))
+                .wypisz(Zmienna.oNazwie('x'))
+                .wypisz(Literal.oWartosci(420))
+                .zadeklarujZmienna('z', Literal.oWartosci(69))
+                .wypisz(Dodawanie.doSiebie(Zmienna.oNazwie('x'), Zmienna.oNazwie('z')))
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("21374202206", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
 }
