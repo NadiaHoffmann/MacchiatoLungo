4a67242f6f7389c00366f039a3cec7ec2745eb61
Nadia Hoffmann <nh448252@students.mimuw.edu.pl>
2023-06-18 18:32:48 +0200

    Przygotuj szablony na testy

--- src/testy/TestyWyrazen.java
+++ src/testy/TestyWyrazen.java
@@ -1,4 +1,119 @@
 package testy;
 
+import instrukcje.BudowniczyProgramu;
+import instrukcje.Program;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestyWyrazen {
+    private final PrintStream standardoweWyjscie = System.out;
+    private final ByteArrayOutputStream ciagNaWyjsciu = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(ciagNaWyjsciu));
+    }
+
+    @Test
+    public void TestDodawanie(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestDzielenie(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestLiteral(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestMnozenie(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestModulo(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestOdejmowanie(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
+
+    @Test
+    public void TestZmienna(){
+        Program program = new BudowniczyProgramu()
+
+                .zbuduj();
+
+        program.wykonaj();
+
+        assertEquals("", ciagNaWyjsciu
+                .toString().trim().replace("\n", "")
+                .replace("\r", "")
+                .replace("Program zakonczyl dzialanie.", ""));
+    }
 }
